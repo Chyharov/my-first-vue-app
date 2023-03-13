6d50a0109d35ad00e4e4ea901d0b5a6abf70d721
Oleksii Chyharov <chyharov@gmail.com>
2023-03-13 11:57:41 +0200

    init

---
+++ dist/js/about.b29ab311.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkmy_first_vue_app"]=self["webpackChunkmy_first_vue_app"]||[]).push([[443],{7381:function(n,u,t){t.r(u),t.d(u,{default:function(){return o}});var a=t(3396);const e={class:"about"},r=(0,a._)("h1",null,"This is an about page",-1),s=[r];function i(n,u){return(0,a.wg)(),(0,a.iD)("div",e,s)}var c=t(89);const f={},p=(0,c.Z)(f,[["render",i]]);var o=p}}]);
+//# sourceMappingURL=about.b29ab311.js.map
